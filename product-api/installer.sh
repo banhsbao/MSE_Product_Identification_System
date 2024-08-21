9e329258db76ccf4c341b6ee1f5c283e6bd70a06
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2024-08-21 23:59:31 +0700

    update

--- product-api/installer.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python3-opencv
-sudo apt-get install python3-pip
-sudo apt-get install python3-rpi.gpio
-pip3 install torch
-pip3 install yolov8
-pip3 install ultralytics
