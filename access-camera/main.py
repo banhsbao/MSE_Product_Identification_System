19e30b24c7321d7036f7d3b8c3ba4d3d44141b81
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2024-08-22 22:31:30 +0700

    change

--- access-camera/main.py
+++ access-camera/main.py
@@ -66,4 +66,4 @@ def step():
     return jsonify({'status': 'Motor moved', 'steps': steps, 'delay': delay})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
